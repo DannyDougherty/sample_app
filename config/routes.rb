2d514778ed5211f5003bfc268963ea1ddea1d417
Danny <Danny.Dougherty@gmail.com>
2013-02-05 14:05:01 -0500

    Added test for cleaned up URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+	get "pages/home"
 
-  get "pages/contact"
-  
-  get "pages/about"
-  
-  get "pages/help"
+	get "pages/contact"
+
+	get "pages/about"
+
+	get "pages/help"
 end
