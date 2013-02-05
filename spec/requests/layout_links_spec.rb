a5fc05fd06b1f5d81c4a5c99a4becc5ff675d2a3
Danny <Danny.Dougherty@gmail.com>
2013-02-05 15:54:04 -0500

    Added test/green for Users controller get new

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-	it "These tests fail because the routes don't work for some reason, however they work in browser" do
+# 	it "These tests fail because the routes don't work for some reason, however they work in browser" do
+# 	end
+	it "should have a Home page at '/'" do
+		get '/'
+		response.should have_selector('title', :content => "Home")
 	end
-# 	it "should have a Home page at '/'" do
-# 		get '/'
-# 		response.should have_selector('title', :content => "Home")
-# 	end
-# 	
-# 	it "should have a Contact page at '/contact'" do
-# 		get '/contact'
-# 		response.should have_selector('title', :content => "Contact")
-# 	end
-# 	
-# 	it "should have an About page at '/about'" do
-# 		get '/about'
-# 		response.should have_selector('title', :content => "About")
-# 	end
-# 	
-# 	it "should have a Help page at '/help'" do
-# 		get '/help'
-# 		response.should have_selector('title', :content => "Help")
-# 	end
+	
+	it "should have a Contact page at '/contact'" do
+		get '/contact'
+		response.should have_selector('title', :content => "Contact")
+	end
+	
+	it "should have an About page at '/about'" do
+		get '/about'
+		response.should have_selector('title', :content => "About")
+	end
+	
+	it "should have a Help page at '/help'" do
+		get '/help'
+		response.should have_selector('title', :content => "Help")
+	end
 end
