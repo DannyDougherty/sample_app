33df771459ad01fb1ad3bf750db6daadac8e5ef1
Danny <Danny.Dougherty@gmail.com>
2013-03-01 15:07:09 -0500

    Finished sign in/out features

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-# 	it "These tests fail because the routes don't work for some reason, however they work in browser" do
-# 	end
 	it "should have a Home page at '/'" do
 		get '/'
 		response.should have_selector('title', :content => "Home")
@@ -41,4 +39,32 @@ describe "LayoutLinks" do
 		click_link "Sign up now"
 		response.should have_selector('title', :content => "Sign up")
 	end
+	
+	describe "when not signed in" do
+		it "should have a signin link" do
+			visit root_path
+			response.should have_selector("a", :href => signin_path)
+		end
+	end
+	
+	describe "when signed in" do
+		before(:each) do
+			@user = Factory(:user)
+			visit signin_path
+			fill_in :email,		:with => @user.email
+			fill_in :password,	:with => @user.password
+			click_button
+		end
+		
+		it "should have a signout link" do
+			visit root_path
+			response.should have_selector("a", :href => signout_path)
+		end
+		
+		it "should have a profile link" do
+			visit root_path
+			response.should have_selector("a", :href => user_path(@user))
+		end
+	end
+	
 end
